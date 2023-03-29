553dde71ae19f6c816f05e740f9468015553761b
mironov <mironov>
2023-03-29 08:46:19 +0400

    fix buildPlugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,22 +6,30 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export function buildPlugin({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
 
-    return [
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css'
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'server',
-            openAnalyzer:  true
-        }),
-    ]
+    const plugins =
+        [
+            new HtmlWebpackPlugin({
+                template: paths.html,
+            }),
+            new webpack.ProgressPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css'
+            }),
+            new webpack.DefinePlugin({
+                __IS_DEV__: JSON.stringify(isDev),
+            })
+        ]
+
+    if (isDev) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin(),
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'server',
+                openAnalyzer:  true
+            }),
+        )
+    }
+
+    return plugins
 }
